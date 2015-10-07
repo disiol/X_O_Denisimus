5824c10639d30ada0f9fa0e1b188a78f4ad1b713
Denis Olenyk <deoniisii@gmail.com>
2015-10-07 22:30:47 +0300

    Добавлены новые переменные, убраны методы

--- src/com/denisimus/GUI/controlerGUI/WinnerControllerGUI.java
+++ src/com/denisimus/GUI/controlerGUI/WinnerControllerGUI.java
@@ -3,175 +3,126 @@ package com.denisimus.GUI.controlerGUI;
 import com.denisimus.CLI.modelCLI.Figure;
 import com.denisimus.GUI.modelGUI.FiledGUI;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: Olenyk Denis (deoniisii@gmail.com)
  * on 31.07.15.
+ *
+ * @version 1.1
  */
 
 public class WinnerControllerGUI {
 
     public Figure getWinner(final FiledGUI filed) {
 
-        checkOfARow1(filed);
-        checkOfARow2(filed);
-        checkOfARow3(filed);
-        checkOfAColumns1(filed);
-        checkOfAColumns2(filed);
-        checkOfAColumns3(filed);
-        checkOfADiagonal(filed);
-        checkOfADiagonal2(filed);
+        // checkOfARow1
+
+        if (check(filed, 0, 1, 2) == true) {
+
+            return filed.getFigure(0);
+
+        }
+
+
+        // checkOfARow2;
+
+
+        if (check(filed, 3, 4, 5) == true) {
+
+            return filed.getFigure(3);
+
+        }
+
+        //checkOfARow3
+
+        if (check(filed, 6, 7, 8) == true) {
+
+            return filed.getFigure(6);
+
+        }
+
+
+        // checkOfAColumns1
+
+        if (check(filed, 0, 3, 6) == true) {
+
+            return filed.getFigure(0);
+
+        }
+
+
+        //checkOfAColumns2
+
+
+        if (check(filed, 1, 4, 7) == true) {
+
+            return filed.getFigure(1);
+
+        }
+
+
+        // checkOfAColumns3;
+
+
+        if (check(filed, 2, 5, 8) == true) {
+
+            return filed.getFigure(2);
+
+        }
+
+
+        //checkOfADiagonal;
+
+
+        if (check(filed, 0, 4, 8) == true) {
+
+            return filed.getFigure(0);
+
+        }
+
+
+        //checkOfADiagonal2;
+
+
+        if (check(filed, 2, 4, 6) == true) {
+
+            return filed.getFigure(2);
+
+        }
+
 
         return null;
     }
 
 
     private boolean check(final FiledGUI filed,
-                          final int currentPoint,
-                          final int nextPoint) {
-        try {
+                          final int coordinate1,
+                          final int coordinate2,
+                          final int coordinate3) {
+        List<Integer> coordinates = new ArrayList<>();
 
+        final Figure currentFigure;
+        final Figure nextFigure;
 
-            final Figure currentFigure;
-            final Figure nextFigure;
+        coordinates.add(coordinate1);
+        currentFigure = filed.getFigure(coordinate1);
+        if (currentFigure == null)
+            return false;
+        coordinates.add(coordinate2);
+        nextFigure = filed.getFigure(coordinate2);
 
+        if (currentFigure != nextFigure)
+            return false;
 
-            currentFigure = filed.getFigure(currentPoint);
-            if (currentFigure == null)
-                return false;
-            nextFigure = filed.getFigure(nextPoint);
+        if (nextFigure != filed.getFigure(coordinate3))
+            return false;
+        coordinates.add(coordinate3);
 
-
-            if (currentFigure != nextFigure)
-                return false;
-
-
-            return check(filed, currentPoint, nextPoint);
-        } catch (StackOverflowError e) {
-
-        }
-
-        return false;
+        return true;
     }
 
-    /**
-     * @param filed
-     * @return
-     */
-    private Figure checkOfARow1(final FiledGUI filed) {
-
-        for (int i = 1; i < 2; i++) {
-            if (check(filed, 0, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-
-        return null;
-    }
-
-    private Figure checkOfARow2(final FiledGUI filed) {
-
-        for (int i = 4; i < 5; i++) {
-            if (check(filed, 3, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-        return null;
-    }
-
-    private Figure checkOfARow3(final FiledGUI filed) {
-
-        for (int i = 7; i < 8; i++) {
-
-            if (check(filed, 6, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-        return null;
-    }
-
-
-    private Figure checkOfAColumns1(final FiledGUI filed) {
-
-        for (int i = 3; i < 6; i += 3) {
-            if (check(filed, 0, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-
-        return null;
-    }
-
-    private Figure checkOfAColumns2(final FiledGUI filed) {
-        for (int i = 4; i < 7; i += 3) {
-
-            if (check(filed, 1, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-
-        return null;
-    }
-
-    private Figure checkOfAColumns3(final FiledGUI filed) {
-        for (int i = 5; i < 8; i += 3) {
-
-            if (check(filed, 2, i)) {
-
-                return filed.getFigure(i);
-
-            }
-
-        }
-
-        return null;
-    }
-
-    private Figure checkOfADiagonal(final FiledGUI filed) {
-        for (int i = 4; i <= 8; i += 4) {
-
-
-            if (check(filed, 0, i)) {
-
-                return filed.getFigure(0);
-
-            }
-        }
-
-        return null;
-    }
-
-    private Figure checkOfADiagonal2(final FiledGUI filed) {
-
-        for (int i = 4; i < 6; i += 2) {
-            if (check(filed, 2, i)) {
-
-                return filed.getFigure(2);
-
-            }
-        }
-
-        return null;
-    }
 
 }
 
